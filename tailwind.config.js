404d677d38a4dbdc277f8d46f35198a0ce39e6d6
3henzijuandri3 <henzijuandri.work@gmail.com>
2023-03-30 22:26:13 +0700

    Home & Detail
    - Home (Completed)
    - Detail (Mobile)

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,11 +23,16 @@ module.exports = {
 
       fontFamily : {
         "poppins" : ["Poppins"],
-        "montserrat" : ["Montserrat"]
+        "montserrat" : ["Montserrat"],
+        "openSans" : ["Open Sans"],
+        "jost" : ["Jost"],
+        "montserratAlt" : ["Montserrat Alternates"]
       },
 
       boxShadow: {
         '3xl': '0px 4px 4px rgba(0, 0, 0, 0.25), 2px 2px 7px 2px rgba(0, 0, 0, 0.25)',
+        'kategori' : "6px 7px 7px -1px rgba(0, 0, 0, 0.25)",
+        'barang' : '8px 9px 6px -1px rgba(0, 0, 0, 0.25)',
       }
     },
   },
